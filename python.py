aa1212a684c4ecff00f6f2443a7a319f85e0dba5
aimlinux <114636143+aimlinux@users.noreply.github.com>
2023-03-18 10:27:37 +0900

    commit

--- python.py
+++ python.py
@@ -4,7 +4,7 @@ import openai
 
 
 # APIキーの設定
-openai.api_key = os.environ["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"]
+openai.api_key = os.environ["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"]
 
 
 response = openai.ChatCompletion.create(
